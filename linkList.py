ac4174aa97426b6700eb70aa1f39ddbd59096732
sadrayavar <sadra.yavarzadeh@gmail.com>
2022-12-30 09:26:58 +0330

    add: init function for main class (database type)

--- linkList.py
+++ linkList.py
@@ -1,68 +1,36 @@
-class Node:
-    def __init__(self,  former, value, next):
-        self.former = former
-        self.value = value
-        self.next = next
-
-
-class LinkList:
+class Library:
+    database = None
     first = None
     last = None
 
-    def __init__(self, value):
-        self.first = Node(None, value,  None)
-        self.last = self.first
+    def __init__(self, databaseName, newFile=False):
+        self.database = databaseName
 
-    def add(self, value):
-        self.last.next = Node(self.last, value, None)
-        self.last = self.last.next
+        if newFile is False:
+            # read database
+            rows = open(self.database, 'r').readlines()
 
-    def edit(self, value, newValue):
-        temp = self.get(value)
-        if (temp == False):
-            return False
+            # search for first and last
+            for i in range(len(rows)):
+
+                # convert string to list
+                rows[i] = self.stringToList(
+                    rows[i])
+                if rows[i][0] == 'None':  # set first
+                    self.first = i
+                if rows[i][1] == 'None':  # set last
+                    self.last = i
+
+            if (self.first == None):
+                print('Database does not have a first book')
+            if (self.last == None):
+                print('Database does not have a last book')
         else:
-            temp.value = newValue
-
-    def delete(self, value):
-        node = self.get(value)
-        if (node == None):
-            return None
-        else:
-            node.former.next = node.next
-            node.next.former = node.former
-
-    def get(self, value):
-        node = self.first
-        while (node != None):
-            if (node.value == value):
-                return node
-            elif (node == None):
-                return None
-            else:
-                node = node.next
-
-    def display(self):
-        node = self.first
-        while (node != None):
-            print(f'{node.former}\t{node.value}\t{node.next}\n')
-            node = node.next
+            try:  # create database
+                open(databaseName, 'x')
+            except:  # delete current database contents
+                open(databaseName, 'w').write('')
+            self.first = 0
 
 
-# tests
-b = LinkList('a')
-b.add('b')
-b.add('c')
-b.add('d')
-b.add('e')
-
-b.edit('c','cc')
-
-# b.delete('c')
-
-b.display()
-
-
-
-#chap d= F0
-#rast b= 50
+b = Library('database.csv', True)
