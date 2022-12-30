b36c79a768a3dd497c626a9ccb534e63f053c1a3
sadrayavar <sadra.yavarzadeh@gmail.com>
2022-12-30 11:28:10 +0330

    add: add method

--- linkList.py
+++ linkList.py
@@ -1,11 +1,14 @@
+# former, next, ISBN, title, author, publisher, publishDate, category, supply, people
+# add remove edit get sort display
+# 2915972317011, 'book name', 'sadra yavarzadeh herisi', 'entesharat sadrayalastLine', 2022012026, ['falsafe', 'comedy', 'dram', 'adventures', 'not adventures'], 999,  [4407469698, 4407469698, 4407469698]
 class Library:
     database = None
     first = None
     last = None
+    emptyRows = []
 
     def __init__(self, databaseName, newFile=False):
         self.database = databaseName
-
         if newFile is False:
             # read database
             rows = open(self.database, 'r').readlines()
@@ -25,12 +28,98 @@ class Library:
                 print('Database does not have a first book')
             if (self.last == None):
                 print('Database does not have a last book')
+            if (self.first == None or self.last == None):
+                exit()
         else:
             try:  # create database
                 open(databaseName, 'x')
             except:  # delete current database contents
                 open(databaseName, 'w').write('')
             self.first = 0
+        print()
+
+    def add(self, ISBN, title, author, publisher, publishDate, category, supply, people):
+        if (True):
+            formerLast = self.last  # save last in temp
+            self.setLast()  # increment last
+            self.editLastBook(formerLast)  # replace None in the next parameter
+            book = self.paramToString(formerLast, None, ISBN, title,
+                                      author, publisher, publishDate, category, supply, people)
+            self.writeToDatabase(book, self.getNext())
+        else:
+            print('book is already exists')
+
+    # helper functions
+
+    def editLastBook(self, formerLast):
+        lastBook = self.readFromDatabase(formerLast)
+        if (lastBook != None):
+            lastBook[1] = self.last
+            l = lastBook
+            tempBook = self.paramToString(l[0], l[1], l[2], l[3], l[4],
+                                          l[5], l[6], l[7], l[8], l[9])
+            self.writeToDatabase(tempBook, formerLast)
+
+    def getNext(self):
+        if (self.last == None):
+            return 2
+        else:
+            if (len(self.emptyRows) == 0):
+                return self.last + 2
+            else:
+                return self.emptyRows[-1]
+
+    def setLast(self):
+        if (self.last == None):
+            self.last = 0
+        else:
+            if (len(self.emptyRows) == 0):
+                self.last += 1
+            else:
+                self.emptyRows.pop()
+
+    def readFromDatabase(self, where):
+        try:
+            rows = open(self.database, 'r').readlines()
+            return self.stringToList(rows[where])
+        except:
+            return None
+
+    def writeToDatabase(self, what, where):
+        rows = open(self.database, 'r').readlines()
+
+        if (len(rows) <= where):
+            rows.append(what)
+        else:
+            rows[where] = what
+
+        file = open(self.database, 'w')
+        file.writelines(rows)
+        file.close()
+
+    def stringToList(self, book):
+        book = book.split(',')
+        book[-1] = book[-1].split(':')
+        book[-3] = book[-3].split(':')
+        for i in range(len(book[-1])):
+            book[-1][i] = int(book[-1][i])
+        for i in range(len(book)-1):
+            try:
+                book[i] = int(book[i])
+            except:
+                pass
+        return book
+
+    def paramToString(self, former, next, ISBN, title, author, publisher, publishDate, category, supply, people):
+        def temp(list):
+            return ':'.join(str(item) for item in list)
+        return f'{former},{next},{ISBN},{title},{author},{publisher},{publishDate},{temp(category)},{supply},{temp(people)}\n'
 
 
 b = Library('database.csv', True)
+b.add(2915972317011, '', '', '', 0, [''], 0,  [1])
+b.add(2915972317011, '', '', '', 0, [''], 0,  [1])
+b.add(2915972317011, '', '', '', 0, [''], 0,  [1])
+b.add(2915972317011, '', '', '', 0, [''], 0,  [1])
+b.add(2915972317011, '', '', '', 0, [''], 0,  [1])
+b.get(2915972317011)
